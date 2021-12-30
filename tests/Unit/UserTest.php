6a5c82039dec9e8dfbd34102b8cb354746182c6d
Jason Tame <jason@advantagelearn.com>
2021-12-30 16:30:49 +0200

    Add helper function for grabbing user check ins

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+use App\Models\CheckIn;
+
+use function PHPUnit\Framework\assertCount;
+
+it('can have multiple check ins', function () {
+    $user = User::factory()->create();
+
+    CheckIn::factory()
+        ->count(3)
+        ->for($user)
+        ->create();
+
+    assertCount(3, $user->checkIns);
+});
