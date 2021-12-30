5586b82ad1ed5cfa521b98e11e719a613a441ae2
Jason Tame <jason@advantagelearn.com>
2021-12-30 14:48:39 +0200

    Install pest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,7 @@
 <?php
 
-namespace Tests\Unit;
+use function PHPUnit\Framework\assertTrue;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
-}
+test('Example', function() {
+    assertTrue(true);
+});
