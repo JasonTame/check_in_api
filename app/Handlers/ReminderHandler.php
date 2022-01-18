4ac1309089b8e74a1fc15cbef96afd9a320437cd
Jason Tame <jason@advantagelearn.com>
2022-01-18 17:09:38 +0200

    Add simple versions of reminder methods

--- app/Handlers/ReminderHandler.php
+++ app/Handlers/ReminderHandler.php
@@ -2,23 +2,66 @@
 
 namespace App\Handlers;
 
-use App\Models\CheckIn;
 use App\Models\Reminder;
 use Carbon\Carbon;
 
 class ReminderHandler
 {
-    public function complete(CheckIn $checkIn): Reminder
+    public function complete(Reminder $reminder): Reminder
     {
-        $reminder = $checkIn->reminder;
-        $reminder->checkin_date = Carbon::now();
+        $intervalDate = $this->getNewCheckInDate($reminder->interval, $reminder->checkin_date);
+
+        $reminder->checkin_date = $intervalDate->format('Y-m-d H:i:s');
+        $reminder->save();
+
+        return $reminder;
     }
 
-    public function snooze()
+    public function snooze(Reminder $reminder, String $snoozeUntil): Reminder
     {
+        $current = Carbon::now();
+
+        switch ($snoozeUntil) {
+            case 'tomorrow':
+                $checkinDate =  $current->addDay();
+                break;
+            case 'end-week':
+                $checkinDate =  $current->endOfWeek();
+                break;
+            case 'next-week':
+                $checkinDate =  $current->addDays(7);
+                break;
+            case 'next-month':
+                $checkinDate =  $current->addMonth();
+                break;
+            default:
+                $checkinDate =  $current->addDay();
+                break;
+        }
+
+        $reminder->checkin_date = $checkinDate->format('Y-m-d H:i:s');
+        $reminder->save();
+
+        return $reminder;
     }
 
-    private function getIntervalDate(String $interval): Carbon
+    private function getNewCheckInDate(String $interval): Carbon
     {
+        $current = Carbon::now();
+
+        switch ($interval) {
+            case 'weekly':
+                return $current->addWeek();
+            case 'bi-weekly':
+                return $current->addWeeks(2);
+            case 'monthly':
+                return $current->addMonth();
+            case 'semi-annually':
+                return $current->addMonths(6);
+            case 'annually':
+                return $current->addYear();
+            default:
+                return $current->addWeek();
+        }
     }
 }
--- tests/Unit/Handlers/ReminderHandlerTest.php
+++ tests/Unit/Handlers/ReminderHandlerTest.php
@@ -1,14 +1,95 @@
 <?php
 
+use App\Handlers\ReminderHandler;
 use App\Models\User;
 use App\Models\CheckIn;
+use Carbon\Carbon;
 
-it('can complete a reminder', function () {
+use function Pest\Laravel\assertDatabaseHas;
+
+beforeEach(function () {
+    Carbon::setTestNow('2022-01-01 12:00:00');
+
     $user = User::factory()
         ->create();
-    $checkIn = CheckIn::factory()
+    $this->reminder = CheckIn::factory()
         ->for($user)
-        ->create();
+        ->create()
+        ->reminder;
+});
 
-    
+it('can complete a reminder', function () {
+    $handler = new ReminderHandler();
+    $handler->complete($this->reminder);
+
+    assertDatabaseHas(
+        'reminders',
+        [
+            'id' => $this->reminder->id,
+            // The default interval is monthly.
+            'checkin_date' => Carbon::now()
+                ->addMonths(1)
+                ->format('Y-m-d H:i:s')
+        ]
+    );
+});
+
+it('can snooze a reminder for a day', function () {
+    $handler = new ReminderHandler();
+    $handler->snooze($this->reminder, 'tomorrow');
+
+    assertDatabaseHas(
+        'reminders',
+        [
+            'id' => $this->reminder->id,
+            'checkin_date' => Carbon::now()
+                ->addDay()
+                ->format('Y-m-d H:i:s')
+        ]
+    );
+});
+
+it('can snooze a reminder until the end of the week', function () {
+    $handler = new ReminderHandler();
+    $handler->snooze($this->reminder, 'end-week');
+
+    assertDatabaseHas(
+        'reminders',
+        [
+            'id' => $this->reminder->id,
+            'checkin_date' => Carbon::now()
+                ->endOfWeek()
+                ->format('Y-m-d H:i:s')
+        ]
+    );
+});
+
+it('can snooze a reminder until the following week', function () {
+    $handler = new ReminderHandler();
+    $handler->snooze($this->reminder, 'next-week');
+
+    assertDatabaseHas(
+        'reminders',
+        [
+            'id' => $this->reminder->id,
+            'checkin_date' => Carbon::now()
+                ->addDays(7)
+                ->format('Y-m-d H:i:s')
+        ]
+    );
+});
+
+it('can snooze a reminder for a month', function () {
+    $handler = new ReminderHandler();
+    $handler->snooze($this->reminder, 'next-month');
+
+    assertDatabaseHas(
+        'reminders',
+        [
+            'id' => $this->reminder->id,
+            'checkin_date' => Carbon::now()
+                ->addMonth()
+                ->format('Y-m-d H:i:s')
+        ]
+    );
 });
